68ed9317347c4392f8fe49f2f345673fa3b4841b
ollepeter <mr.peterolle@gmail.com>
2017-07-02 22:17:18 +0200

    registration process works still in progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-# import functions
+import functions
 
 
 app = Flask(__name__)
@@ -8,6 +8,27 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/registration')
+def registration():
+        return render_template("registration.html")
+
+
+@app.route('/registration_process', methods=['POST'])
+def register_process():
+    userdata = request.form
+    username = userdata['username']
+    password = userdata['password']
+    if username and password:
+        update_result = functions.database_update(username, password)
+        if update_result == 'OK':
+            success_message = "Hello " + username + " ! You have succesfully registered!"
+            return jsonify({'successmessage': success_message})
+        else:
+            return jsonify({'errormessage': update_result})
+    else:
+        return jsonify({'errormessage': 'Missing data!'})
 
 
 if __name__ == '__main__':
